028a1adfc995298650493de13968012edcd17ea0
Osama kandel <okandel.dev@gmail.com>
2021-06-15 12:14:26 +0200

    solve getUser

--- services/OrderSystem/controllers/UserController.js
+++ services/OrderSystem/controllers/UserController.js
@@ -9,8 +9,7 @@ export class UserController {
     const user = await this.userService.getUserByPhone(mobile)
     res.send({
       statusCode: 200, 
-      user: user,
-      req:req
+      user: user
     }) 
   }
 } 
