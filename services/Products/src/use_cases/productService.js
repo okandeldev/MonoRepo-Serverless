5dc0b4d292898727fef393140d2eb2ca6eaf97f4
Osama kandel <okandel.dev@gmail.com>
2021-06-22 16:59:44 +0200

    suggestedProducts

--- services/Products/src/use_cases/productService.js
+++ services/Products/src/use_cases/productService.js
@@ -15,7 +15,7 @@ export class productService {
     let products = await this.productRepository.findAll(keyword.trim());
     const ExcatProduct = (products.filter((p)=> p.name.toLowerCase() == keyword.toLowerCase()))
     let matchedProduct = (ExcatProduct.length== 1)? ExcatProduct[0] : null;  
-    let suggestedProducts = (ExcatProduct.length == 0 && products.length >1)? products : [];
+    let suggestedProducts = (ExcatProduct.length == 0)? products : [];
       
     return {
       matchedProduct,
