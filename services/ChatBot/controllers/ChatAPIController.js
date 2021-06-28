3198b5294605f2cbfb1dd75b3d15d3689df385dc
Osama kandel <okandel.dev@gmail.com>
2021-06-28 14:32:01 +0200

    no message

--- services/ChatBot/controllers/ChatAPIController.js
+++ services/ChatBot/controllers/ChatAPIController.js
@@ -9,22 +9,22 @@ export class ChatAPIController {
   async webhook(req, res) {
     const data =   req.body; 
     await this.mongoDao.insertOne("chatbotAPI", data);
-    for (let i in data.messages) { 
-        const author = data.messages[i].author; //ex. 17472822486@c.us 
-        const mobile = author.replace("@c.us", "");
-        const body = data.messages[i].body;
-        const chatId = data.messages[i].id;
-        const senderName = data.messages[i].senderName;
-        if(data.messages[i].fromMe)return; 
+    // for (let i in data.messages) { 
+    //     const author = data.messages[i].author; //ex. 17472822486@c.us 
+    //     const mobile = author.replace("@c.us", "");
+    //     const body = data.messages[i].body;
+    //     const chatId = data.messages[i].id;
+    //     const senderName = data.messages[i].senderName;
+    //     if(data.messages[i].fromMe)return; 
 
-        let chatSessionData = await this.chatBotService.getAuthorChatBotSessionData(chatId,author,mobile);
-        chatSessionData = chatSessionData ||  {chatId:chatId}   
-        const updatedChatSessionData = await processRecivedMessage(body,chatSessionData);
-        if (updatedChatSessionData){
-          // chatSessionData = updatedChatSessionData || chatSessionData
-          await this.chatBotService.updateAuthorChatBotSessionData(author,updatedChatSessionData.chatSessionData); 
-        }
-    }
+    //     let chatSessionData = await this.chatBotService.getAuthorChatBotSessionData(chatId,author,mobile);
+    //     chatSessionData = chatSessionData ||  {chatId:chatId}   
+    //     const updatedChatSessionData = await processRecivedMessage(body,chatSessionData);
+    //     if (updatedChatSessionData){
+    //       // chatSessionData = updatedChatSessionData || chatSessionData
+    //       await this.chatBotService.updateAuthorChatBotSessionData(author,updatedChatSessionData.chatSessionData); 
+    //     }
+    // }
     res.send({
       statusCode: 200,
       data: data,
