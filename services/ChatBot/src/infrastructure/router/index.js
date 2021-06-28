144271c3d0b66fa1ed686ad080d3f7df22819472
Osama kandel <okandel.dev@gmail.com>
2021-06-28 16:49:45 +0200

    no message

--- services/ChatBot/src/infrastructure/router/index.js
+++ services/ChatBot/src/infrastructure/router/index.js
@@ -2,7 +2,7 @@ const { container, setup } = require('../config/di-setup');
 const ChatAPIController = container.resolve('ChatAPIController');    
  
 export let routers = function (app) { 
-  app.post("/",(req, res) => { 
+  app.post("/webhook",(req, res) => { 
     ChatAPIController.webhook(req, res)
   }); 
 };
